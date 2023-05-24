9ada26ed8d05aa8fcc26b963c1fd1e55a14d44ba
Wanderson.jpeg <81868579+WandersonSantoos@users.noreply.github.com>
2023-05-24 16:18:13 -0300

    Novas funcionalidades foram adicionadas

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-// Variáveis dos botões
-let BotaoAdicionar = document.querySelector('.btn-adicionar')
+// Variáveis dos botões das encomendas
+
+let BotaoAdicionar = document.querySelector('.btn-adicionar2')
+let BotaoAdicionar3 = document.querySelector('.btn-adicionar3')
+let BotaoAdicionar4 = document.querySelector('.btn-adicionar4')
+let BotaoAdicionar5 = document.querySelector('.btn-adicionar5')
+
 let BotaoRemover = document.querySelector('.btn-remover')
+let BotaoRemover3 = document.querySelector('.btn-remover3')
+let BotaoRemover4 = document.querySelector('.btn-remover4')
+let BotaoRemover5 = document.querySelector('.btn-remover5')
+
+// container encomendas
 let container2 = document.querySelector('.container2')
+let container3 = document.querySelector('.container3')
+let container4 = document.querySelector('.container4')
+let container5 = document.querySelector('.container5')
+
+// variaveis que some o botão após o click
 let sumirbotao = document.querySelector('.adicionar')
+let sumirbotao3 = document.querySelector('.adicionar3')
+let sumirbotao4 = document.querySelector('.adicionar4')
+let sumirbotao5 = document.querySelector('.adicionar5')
 class FormSubmit {
     constructor(settings) {
       this.settings = settings;
@@ -72,13 +90,78 @@ class FormSubmit {
     }
   })
 
+  // Adiciona a div da encomenda 3
+  BotaoAdicionar3.addEventListener('click', function() {
+    if (container3.style.display === 'block') {
+      container3.style.display = 'block'
+      sumirbotao3.style.display = 'none'
+    } else {
+      sumirbotao3.style.display = 'none'
+      container3.style.display = 'block';
+    }
+  })
+
+  // Adiciona a div da encomenda 4
+  BotaoAdicionar4.addEventListener('click', function() {
+      if (container4.style.display === 'block') {
+        container4.style.display = 'block'
+        sumirbotao4.style.display = 'none'
+      } else {
+        sumirbotao4.style.display = 'none'
+        container4.style.display = 'block';
+      }
+  })
+
+  // Adiciona a div da encomenda5
+  BotaoAdicionar5.addEventListener('click', function() {
+    if (container5.style.display === 'block') {
+      container5.style.display = 'block'
+      sumirbotao5.style.display = 'none'
+    } else {
+      sumirbotao5.style.display = 'none'
+      container5.style.display = 'block';
+    }
+  })
+
   // Remove a div da encomenda 2
   BotaoRemover.addEventListener('click', function() {
     if (container2.style.display === 'none') {
       container2.style.display = 'block'
     } else {
       container2.style.display = 'none';
-      sumirbotao.style.display = 'flex'
+      container3.style.display = 'none';
+      container4.style.display = 'none';
+      container5.style.display = 'none';
+    }
+  })
+
+  // Remove a div da encomenda 3
+  BotaoRemover3.addEventListener('click', function() {
+    if (container3.style.display === 'none') {
+      container3.style.display = 'block'
+    } else {
+      container3.style.display = 'none';
+      container4.style.display = 'none';
+      container5.style.display = 'none';
+    }
+  })
+
+  // Remove a div da encomenda 4
+  BotaoRemover4.addEventListener('click', function() {
+    if (container4.style.display === 'none') {
+      container4.style.display = 'block'
+      container5.style.display = 'none';
+    } else {
+      container4.style.display = 'none';
+    }
+  })
+
+  // Remove a div da encomenda 5
+  BotaoRemover5.addEventListener('click', function() {
+    if (container5.style.display === 'none') {
+      container5.style.display = 'block'
+    } else {
+      container5.style.display = 'none';
     }
   })
   
